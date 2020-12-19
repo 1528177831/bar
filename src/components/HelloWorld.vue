5298a6e4e567f266d95c3020497cd3bc5f405e86
崔文刚 <1528177831@qq.com>
2020-12-19 13:10:11 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}World</h1>
+    <infinite-slide-bar :barStyle="{ background: '#fff7cc', padding: '5px 0' }">
+      <span style="color: #f60"
+        >Your time is limited, so don't waste it living someone else's
+        life.</span
+      >
+    </infinite-slide-bar>
+  </div>
+</template>
+
+<script>
+import InfiniteSlideBar from 'vue-infinite-slide-bar'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hello'
+    }
+  },
+  components: {
+    InfiniteSlideBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  font-weight: normal;
+}
+</style>
